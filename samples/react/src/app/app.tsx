8e75f17ee4d2b21277fd48e00dd39be26de50a93
Konrad Koch <konrad@labk.it>
2020-10-07 22:11:06 +0200

    Added react base application

---
+++ samples/react/src/app/app.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './app.scss';
+
+export class App extends Component {
+  render() {
+    return <div className="app"></div>;
+  }
+}
+
+export default App;
