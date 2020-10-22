749b3789c9396e4fcaf7c7e2965a92594dc7f807
Konrad Koch <konrad@labk.it>
2020-10-22 17:54:06 +0200

    Fixed linting error

--- samples/react-e2e/src/integration/app.spec.js
+++ samples/react-e2e/src/integration/app.spec.js
@@ -2,20 +2,15 @@ describe('react', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should show two input nodes', () => {
-    const node1 = cy.get('#node-1');
-    const node2 = cy.get('#node-2');
-
-    node1.should('exist');
-    node2.should('exist');
+    cy.get('#node-1').should('exist');
+    cy.get('#node-2').should('exist');
 
     cy.get('#node-1 > .control > input').should('have.value', 2);
     cy.get('#node-2 > .control > input').should('have.value', 3);
   });
 
   it('should have one add node', () => {
-    const add = cy.get('#node-3');
-    add.should('exist');
-
+    cy.get('#node-3').should('exist');
     cy.get('#node-3 > .control > input').should('have.value', 5);
   });
 
