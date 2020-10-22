5c3a53325982cf5fbc15e428863925ea5ce5e88c
Konrad Koch <konrad@labk.it>
2020-10-22 17:10:39 +0200

    Fixed path

--- samples/react/src/main.js
+++ samples/react/src/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './app/app';
+import App from './app/App';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
