f9e30010214068019c12728ae7d769da846ed990
Konrad Koch <konrad@labk.it>
2021-05-03 13:37:59 +0200

    Add arrange event to type definition

--- packages/arrange-plugin/src/lib/events.ts
+++ packages/arrange-plugin/src/lib/events.ts
@@ -1,5 +1,7 @@
+import { Node } from '@naetverkjs/naetverk';
+
 declare module '@naetverkjs/naetverk/src/lib/events' {
   export interface EventsTypes {
-    arrange: void;
+    arrange: Node[];
   }
 }
